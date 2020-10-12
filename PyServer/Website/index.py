8c335665c146505b679426ebd6d723392e0559a9
欣普 <15877161799@163.com>
2020-10-12 21:58:38 +0800

    website index file.

---
+++ PyServer/Website/index.py
@@ -0,0 +1,13 @@
+print("""
+<html>
+<head>
+  <title>My first Python page</title>
+  <meta charset='utf-8'>
+</head>
+<body>
+  <center>
+    <h1>Hello Python!</h1>
+    <p>This is my first Python page.</p>
+  </center>
+</body>
+</html>""")
