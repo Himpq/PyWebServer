141cd03b439dd0a3e286b3102891d81ba7b7777b
欣普 <15877161799@163.com>
2020-10-12 21:55:41 +0800

    Create http_returntype.py

---
+++ PyServer/http_returntype.py
@@ -0,0 +1,50 @@
+return_filetype = {           #返回某文件后缀时的类型
+    "text.html"               : ['html', 'htm', 'py', 'jsp'],
+    "text.xml"                : ['xml', '323', 'biz', 'cml'],
+    "text.plain"              : ['txt', 'text', 'css', 'js'],
+    'text.asp'                : ['asp'],
+    'text.csv'                : ['csv'],
+    
+    "image.png"               : ['png'],
+    'image.jpeg'              : ['jpg', 'jpeg', 'jfif', 'jpe'],
+    'image.gif'               : ['gif'],
+    'image.tiff'              : ['tiff', 'tif'],
+    'image.fax'               : ['fax'],
+    'image.x-icon'            : ['ico', 'icon'],
+
+    'java.*'                  : ['class', 'java'],
+    
+    'application.msword'      : ['doc', 'docx'],
+    'application.pdf'         : ['pdf'],
+    'application.octet-stream': ['exe', 'bat'],
+    'application.json'        : ['json'],
+    'application.ogg'         : ['ogg'],
+    'application.font-woff'   : ['woff'],
+    'application.zip'         : ['zip'],
+    'application.x-ppt'       : ['ppt'],
+    'application.vnd.android.package-archive' : ['apk'],
+
+    'audio.mp2'               : ['mp2'],
+    'audio.mp3'               : ['mp3'],
+    'audio.mp4'               : ['mp4'],
+    'audio.mp1'               : ['mp1'],
+    'audio.rn-mpeg'           : ['mpga'],
+    'audio.wav'               : ['wav'],
+    'audio.x-ms-wma'          : ['wma'],
+    'audio.mid'               : ['mid', 'midi'],
+    'audio.mpegurl'           : ['m3u'],
+
+    'video.x-ms-asf'          : ['asf', 'asx'],
+    'video.avi'               : ['avi'],
+    'video.x-mpeg'            : ['m1v', 'm2v'],
+    'video.mpeg4'             : ['m4e', 'mp4'],
+    'video.x-mpg'             : ['mpa'],
+    'video.x-ms-wm'           : ['wm'],
+    'video.x-ms-wmv'          : ['wmv'],
+    'video.x-ms-wvx'          : ['wvx'],
+    'video.mpeg'              : ['mpeg'],
+    'video.x-mpeg'            : ['mps', 'm2v'],
+    'video.x-ms-wmx'          : ['wmx'],
+    
+    'default': 'text.plain'
+}
