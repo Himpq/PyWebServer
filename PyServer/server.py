1e2e58f4f304c1effd06432a996defb9677b0646
欣普 <15877161799@163.com>
2021-01-02 20:26:40 +0800

    Update server.py

--- PyServer/server.py
+++ PyServer/server.py
@@ -1,8 +1,13 @@
+# -*- encoding: utf-8 -*-
 """
     PyServer
     By Himpq|2020-10-12
 """
 from threading import Thread
+from Logger import Logger
+from server_config import *
+from ExpHttpData import *
+from functions import *
 import socket
 import time
 import datetime
@@ -13,99 +18,98 @@ import os
 import re
 import CacheModule as TM
 import urllib.parse as uparse
-import wmi
-import chardet
-from Logger import Logger
-from server_config import *
-from ExpHttpData import *
+#import chardet
 import inspect
 import ctypes
-from memory_profiler import profile
+import ssl, gzip
+from io import *
+from memory_profiler import *
 
-Logger = Logger()
+prof = profile
+
+def profile(func):
+    def x(*arg, **args):
+        l = time.time()
+        o = func(*arg, **args)
+        p = time.time()
+        #sys.stderr.write("===========\n函数"+func.__name__+" 耗时:"+str(p-l)+"\n==========\n")
+        return o
+    return x
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
 SERVER = "PyServer"
 VERSION = "/2.1.31"
 
-##导入PyFDB
-if config['use_pyfdb']:
-    sys.path.append("./PyFileDatabase")
-    try:
-        import pyfiledb
-        pyfiledb.start()
-    except:
-        Logger.warn("Cannot found the PyfileDatabase. Please check your installed.")
-    else:
-        Logger.comp("Import PyFileDatabase successful.")
-else:
-    Logger.error("PyFileDatabase was disabled.")
 
-#功能类
-def _async_raise(tid, exctype):
-    """Raises an exception in the threads with id tid"""
-    if not inspect.isclass(exctype):
-        raise TypeError("Only types can be raised (not instances)")
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), ctypes.py_object(exctype))
-    if res == 0:
-        raise ValueError("invalid thread id")
-    elif res != 1:
-        # """if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect"""
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
-
-
-def stop_thread(thread):
-    _async_raise(thread.ident, SystemExit)
-def DeleteObjectAllProperties(objectInstance):
-    if not objectInstance:
-        return
-    listPro =[key for key in objectInstance.__dict__.keys()]
-    for key in listPro:
-        objectInstance.__delattr__(key)
-def get_memory():
-    global server_memory
-    pythoncom.CoInitialize()
-
-    a = wmi.WMI()
-    while 1:
-        server_memory = [int(a.Win32_ComputerSystem()[0].TotalPhysicalMemory),
-                        int(a.in32_OperatingSystem()[0].FreePhysicalMemory)]
-        time.sleep(config['reload_information_time'])
-def dict_inone(*arg):
-    x = {}
-    for i in arg:
-        for f in i:
-            x[f] = i[f]
-    return x
+def gzip_encode(data):
+    buf = BytesIO()
+    f = gzip.GzipFile(mode='wb', fileobj=buf)
+    f.write(data)
+    f.close()
+    buf.seek(0)
+    return buf.read()
 
 #服务器
-
-
 class Server:
+    @profile
     def __init__(self, ip='localhost', port=80, maxlisten=128):
+        self.is_start = False
         self.ip = ip
         self.port = port
         self.maxlisten = maxlisten
-        self.is_start = False
-
         self.tpool = {}
+        self.ssl = False
 
-    def __setattr(self, key, val):
-        if self.is_start:
+    def open_SSL(self):
+        self.ssl = True
+        self.ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        self.ssl_context.load_cert_chain(certfile=sslpath[0], keyfile=sslpath[1])
+        #self.ssl_context.load_verify_locations(sslpath[2])
+        #self.ssl_context.verify_mode = ssl.CERT_REQUIRED
+        
+    def __setattr__(self, key, val):
+        if 'is_start' in self.__dict__ and self.is_start:
             raise KeyError("Cannot change any arguments when server is running.")
         self.__dict__[key] = val
 
     def _accept(self):
         ident = 0
         while 1:
-            conn, addr = self.socket.accept()
+            try:
+                conn, addr = self.socket.accept()
+                if self.ssl:
+                    co = conn
+                    conn = self.ssl_context.wrap_socket(co, server_side=True)
+                se = ServerResponse
+                arg = se(conn, addr, ident, self)
+                user = Thread(target=arg.response)
+                user.start()
+                print("Recv from",addr)
+                self.tpool[ident] = [user, se]
+                ident += 1
+            except Exception as e:
+                print(e)
+                continue
 
-            user = Thread(target=ServerResponse, args=(conn, addr, ident, self))
-            user.start()
-            self.tpool[ident] = user
-            ident += 1
+            #time.sleep(0.01)
 
-            time.sleep(0.01)
+    def _res(self):
+        while 1:
+            time.sleep(0.1)
+            try:
+                for i in list(self.tpool.keys()):
+                    if self.tpool[i][0].is_alive():
+                        continue
+                    #self.tpool.pop(i)
+                    del self.tpool[i]
+            except:
+                continue
+
+    def _ke(self):
+        while 1:
+            Logger.info("Server alive.")
+            time.sleep(10)
 
     def start(self):
         self.socket = socket.socket()
@@ -113,69 +117,131 @@ class Server:
         self.socket.listen(self.maxlisten)
         self.accept_thread = Thread(target=self._accept)
         self.accept_thread.start()
+        self.clear_thread = Thread(target=self._res)
+        self.clear_thread.start()
+        #self.keep_alive = Thread(target=self._ke)
+        #self.keep_alive.start()
         Logger.comp("Server is running from now on.")
         self.is_start = True
 
+    def stop(self):
+        stop_thread(self.accept_thread)
+        stop_thread(self.clear_thread)
+        self.socket.close()
+
 class ServerResponse:
     def __init__(self, conn, ip, ident, server):
+        
         self.server = server
         self.ident = ident
         self.conn = conn
+        self.connfile = conn.makefile('rb')
         self.ip = ip
+        self.cachesize = 4096
         self.cache2 = File_B()
 
-        Logger.comp("Accept ip:", ip)
+        Logger.comp("响应IP: ", ip)
+        self.runin = ''
+        self._DATA = {}
+        self._FILE = {}
 
-        self.response()
-        stop_thread(server.tpool[self.ident])
-        del server.tpool[self.ident]
-        DeleteObjectAllProperties(self)
+        self.status = 'in init func.'
+
+        '''try:
+            #self.response()
+            pass
+        except Exception as e:
+            Logger.warn(e)
+        finally:
+            try:
+                self.connfile.close()
+                self.conn.close()
+            except:
+                pass
+            try:
+                while 1:
+                    stop_thread(self.server.tpool[self.ident][0])
+            except:
+                pass
+        #stop_thread(server.tpool[ident+1])'''
 
     def response(self):
-
-        data = exp(exp2(self.conn))
-
-        self.data = data
-        Logger.info(data)
-
         time1 = time.time()
+        self.status = 'in response func.'
+        self.data = ''
+        self.ysdata = ''
+        def getx():
+            ysdata , data = exp(self.connfile)
+            self.data = data
+            self.ysdata = ysdata
+        self.getdatathread = Thread(target=getx)
+        self.getdatathread.start()
+        n = 0
+        x = False
+        while self.ysdata == '' or self.data == '':
+            if n == 1500:
+                try:
+                    stop_thread(self.getdatathread)
+                except:
+                    pass
+                finally:
+                    x= True
+                    break
+            n += 1
+            time.sleep(0.01)
+        if x:
+            print("Time out.")
+            return 0
 
-        if data.get("content_type", None) == 'multipart/form-data':
-            self.updata(data)
+        data = self.data
+        ysdata = self.ysdata
+        if data.get("host") == None or data.get("path") == None:
+            return
+        Logger.info("请求路径: ", data['host'] + data['path'])
+
+        if not data.get("host", None) in bind_domains and not isIPv4(data.get("host")):
+            self.err("内部错误", "未绑定的域名，请在配置中添加。")
             return
 
-        try:
-            if data['path'][-3:] == '.py':
-                self.py(data)
-            elif os.path.isdir(ServerPath+"/"+data['path']):
-                for i in config['default-page']:
-                    if os.path.isfile(ServerPath+"/"+data['path']+'/'+i):
-                        data['path'] = data['path']+'/'+i
-                        self.data = data
-                        break
-                if i[-3:] == '.py':
+        if data.get("content-type", None) == 'multipart/form-data':
+            self.updata(data)
+        else:
+            try:
+                if data['path'][-3:] == '.py':
                     self.py(data)
+                elif os.path.isdir(ServerPath+"/"+data['path']):
+                    for i in config['default-page']:
+                        if os.path.isfile(ServerPath+"/"+data['path']+'/'+i):
+                            data['path'] = data['path']+'/'+i
+                            self.data = data
+                            break
+                    if i[-3:] == '.py':
+                        self.py(data)
+                    else:
+                        self.fl(data)
                 else:
                     self.fl(data)
-            else:
-                self.fl(data)
-        except Exception as e:
-            Logger.error(e, "(at line", e.__traceback__.tb_lineno,")")
-        time2 = time.time()
-        Logger.warn("Response time: %ss"%str(time2-time1))
-        
+            except Exception as e:
+                Logger.error(e, "(at line", e.__traceback__.tb_lineno,")")
 
-    
-    def set_cookie(self, key, val):
-        self.header.set("Set-Cookie", "%s=%s; Expires=%s;", True)
-    def set_header(self, val):
-        v = str(val).split(":")
-        if len(v) == 1:
-            self.cache.append(val)
-            return
-        key = v[0]
-        val = ':'.join(v[1:])
-        self.cache.write(key, str(val)+"\r\n")
+        time2 = time.time()
+
+        Logger.comp("响应结束。响应时间: %ss"%(time2-time1))
+        self.status = 'kill me.'
+
+        
+        try:
+                self.connfile.close()
+                self.conn.close()
+        except:
+                pass
+        
+    def set_cookie(self, key, val, expires=3600):
+        self.header.set("Set-Cookie", "%s=%s; Expires=%s;"%(key, val, expires), True)
+    def set_header(self, key, val):
+        self.header.set(key, str(val))
+    def set_statuscode(self, code, content): #404, File not found
+        self.header.set(0, "HTTP/1.1 "+str(code)+" "+content)
     def set_html(self, *val, **arg):
         #self.cache2.write(str(val))
         print(*val, **arg, file=self.cache2)
@@ -183,6 +249,7 @@ class ServerResponse:
         globals_var = {}
         globals_var['set_cookie'] = self.set_cookie
         globals_var["set_header"] = self.header.set
+        globals_var['set_statuscode'] = self.set_statuscode
         globals_var["print"] = self.set_html
         globals_var['include'] = self.include
         globals_var['_POST'] = self.data['postdata']
@@ -195,47 +262,48 @@ class ServerResponse:
         globals_var["_DATA"] = {}
         return globals_var
 
+    @profile
     def updata(self, data):
-        if 'boundary' in self.data:
+        self.status = 'in updata func.'
+        if data.get("boundary"):
             if data['path'][-3:] == '.py':
-                #self.py(data, tocls(updata_exp(self.conn, "--"+data['boundary'])))
-        
-                pass
-            x = updata_exp(self.conn, data['boundary'])
+                x = updata_exp(self.connfile, data['boundary'])
+                if not len(x) == 0:
+                    do = {"_FILE":x[0], "_DATA":x[1]}
+                else:
+                    do = {}
+                self.py(data, do)
+                return
+            x = updata_exp(self.connfile, data['boundary'])
             self.py(data, {"_FILE":x[0], "_DATA":x[1]})
-        #print(self.conn.recv(1024))
-                
-        self.conn.close()
         return
 
+    @profile
     def py(self, data, glo={}):
+        self.status = 'in py func.'
+
+        if config['python'] == False:
+            return
         if os.path.isfile(ServerPath+"/"+data['path']):
             ccc = ServerPath+"/"+data['path']
             with open(ccc, 'rb') as f:
+                #"import os; os.chdir('{0}'); del os;\n".format(ServerPath+"/"+os.path.split(data['path'])[0]).encode()+
                 u = f.read()
-
-                #j = chardet.detect(u)['encoding']
-                #u = u.decode(j)
                 
             self.header = Header()
-
-            globals_var = self.getGlobal()
+            globals_var = self.getGlobal().copy()
             self.var = globals_var
 
             for i in glo:
                 self.var[i] = glo[i]
-            h = {}
+
             try:
-                u = compile(u, ccc, 'exec')
-                exec(u, globals_var, h)
-                del h, globals_var, u
+                u = compile(u, '', 'exec')
+                exec(u, self.var)
+
+                del globals_var, u
             except Exception as e:
-                header = Header()
-                header.set("Content-Type", "text/html; charset=utf-8")
-                
-                self.conn.send(header.encode()+b'\r\n')
                 self.err("codeerror", e)
-                self.conn.close()
                 return
 
             self.cache2.save()
@@ -244,61 +312,93 @@ class ServerResponse:
             datas += self.cache2.read().encode()+b'\r\n\r\n'
 
             self.conn.send(datas)
-            self.conn.close()
-            #self.conn.shutdown(socket.SHUT_RDWR)
+        else:
+            self.err("404")
 
+    @profile
     def fl(self, data):
+        self.status = 'in fl func.'
         if data['path'][-3:] == '.py':
             self.py(data)
             return
         if os.path.isfile(ServerPath+"/"+data['path']):
+            tsize = os.path.getsize(ServerPath+"/"+data['path'])
+            size = tsize
             header = Header()
-            header.set("Content-Type", FileType(data['path']))
-            header.set("Content-Length", os.path.getsize(ServerPath+"/"+data['path']))
 
-            self.conn.send(b'HTTP/1.1 200 Ok!\r\n')
-            self.conn.send(header.encode()+b'\r\n')
+            if 'range' in data:
+                ranges = getRange(data['range'].split("=")[1].strip())
+
+                size = (ranges[1] + 1 if not ranges[1] == '' else size) - ranges[0]
+                header.set("Content-Range", "bytes %s-%s/%s"%(ranges[0], ranges[1] if not ranges[1] == '' else tsize-1, tsize))     
+                header.set(0, "HTTP/1.1 206 Partial Content")
+                    
+            header.set("Content-Type", FileType(data['path']))
+            header.set("Content-Length", "%s"%(size if not 'range' in data else (ranges[1] if not ranges[1] == '' else tsize)-ranges[0]))
+            #header.set("Content-Encoding", "gzip")
+
+            #self.conn.send(header.encode()+b'\r\n')
+            h = header.encode()+b'\r\n'
 
             with open(ServerPath+"/"+data['path'], 'rb') as f:
-                b = f.read(1024*100)
-                while not b == b'':
-                    self.conn.send(b)
-                    b = f.read(1024*100)
+                if 'range' in data:
+                    f.seek(ranges[0])
+                    d = f.read((ranges[1] if not ranges[1] == '' else tsize) + 1)
+                    self.conn.send(h+d)
+                    return
 
-            self.conn.close()
+                try:
+                    x = f.read()
+                    self.conn.sendall(h+x)
+
+                except Exception as e:
+                    print(e)
+                    
+                    
         else:
-            header = Header()
-            self.conn.send(header.encode()+b'\r\n')
-            
             self.err('404')
-            self.conn.shutdown()
 
+    @profile
     def err(self, ty, x=''):
+        header = Header()
+        header.set("Content-Type","text/html")
         if ty == 'codeerror':
+            header.set(0, "HTTP/1.1 500 Code Error")
+            self.conn.send(header.encode()+b'\r\n')
             self.conn.send(b"<html><body><center><h1>Python Error</h1>")
-            self.conn.send(b"<br><font color='red'>Error: </font>"+str(x).encode()+b"</body></html")
+            self.conn.send(b"<br><font color='red'>Error: </font>"+str(x).encode()+b"(at line "+str(x.__traceback__.tb_lineno).encode()+b")</body></html>")
             return
-        self.conn.send(ERRCODE(ty))
+        if not str(ty) in http_errorcodes:
+            header.set(0, "HTTP/1.1 500 Service Error")
+            self.conn.send(header.encode()+b'\r\n')
+            self.conn.send((ERRPage % ('500', ty, x)).encode())
+        else:
+            header.set(0, "HTTP/1.1 %s %s"%(ty, http_errorcodes[str(ty)][0]))
+            self.conn.send(header.encode()+b'\r\n')
+            self.conn.send((ERRPage % (ty, http_errorcodes[str(ty)][0], http_errorcodes[str(ty)][1])).encode())
 
-    #@profile
+    @profile
     def include(self, path):
         pathx=ServerPath+"/"+os.path.split(self.data['path'])[0]
         path = pathx+"/"+path
 
         if os.path.isfile(path):
-            with open(path, 'r') as f:
+            with open(path, 'r', encoding='utf-8') as f:
                 u = f.read()
                 
-            globals_var = self.getGlobal()
+            #globals_var = self.getGlobal()
+            globals_var = self.var
             s = {}
 
             exec(("import sys;sys.path.append('%s')\n"%pathx)+u, globals_var)
 
-            name = os.path.split(path)[1].split('.')[0]
-            self.var[name] = Module(dict_inone(s, globals_var))
+            """name = os.path.split(path)[1].split('.')[0]
+            self.var[name] = Module(dict_inone(s, globals_var))"""
+            x = dict_inone(s, globals_var)
+            for i in x:
+                self.var[i] = x[i]
         else:
-            raise ImportError("Cannot find module in path: '%s'"%path)
-            #print("Module cannot find.", file=sys.stderr)
+            self.err("codeerror", "Cannot find module in path: '%s'"%path)
 
 class Module:
     def __init__(self, dicts):
@@ -312,6 +412,7 @@ class Header:
         self.headers["Content-Encoding"] = "identity"
         self.headers["Server"] = SERVER+VERSION
         self.headers["Date"] = time.asctime()
+        self.headers["Accept-Ranges"] = "bytes"
         
     def set(self, key, val, diejia=False):
         if key in self.headers:
@@ -362,5 +463,11 @@ class File_C:
     def append(self, value):
         self.file[len(self.file)] = value
 
-a=Server(ip='192.168.1.104')
+a=Server(ip='IP')
 a.start()
+
+#OPEN WITH SSL
+b=Server(ip='IP', port=443)
+b.open_SSL()
+b.start()
+
