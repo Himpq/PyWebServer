410a8c1f65355f75a7a2463921b52b6e19256fed
欣普 <15877161799@163.com>
2022-08-09 00:27:46 +0800

    更新有关于h2的配置

--- PyWebServer/server_config.py
+++ PyWebServer/server_config.py
@@ -22,12 +22,17 @@ default-page=('index.html','index.htm','index.py')
 python=true
 raise-error=true
 timeout=5
+keep-alive-max=20
 cachesize=409600
 sameip-request-count=5
-server_status=NORMAL
-errorpagePath=./ErrorPages/error.html
-sslpath=('cert.crt','key.key','ca.crt')
+server-status=NORMAL
+errorpage-path=./ErrorPages/error.html
+ssl-path=('cert.crt','key.key','ca.crt')
+ssl-password=
+ssl-doshakehand=True
 maxsize-for-etag = (1024*1024*100)
+enable-plugin=true
+support-protocols=(["spdy/3.1", "http/1.1"])
 
 [black_list]
 blacklist=
@@ -52,18 +57,23 @@ SAFEGUARD = 0 #维护
 NORMAL = 1    #正常
 
 config = {}
+setting = {}
 http_errorcodes = {}
 
+#[setting]内容会定义于全局变量
 for i in opts['setting'].keys():
     globals()[i] = opts['setting'][i]
+    setting[i]   = opts['setting'][i]
 
 for i in opts['config']:
+#[config]内容会定义于dict config
     config[i] = opts['config'][i]
-#prettyPrint(config)
+
 
 for i in opts['http_errorcodes']:
+#[http_errorcodes]会定义于dict http_errorcodes
     http_errorcodes[i] = opts['http_errorcodes'][i]
-#prettyPrint(http_errorcodes)
+
 
 black_list = [] #IP黑名单
 bind_domains = [] #绑定的域名
