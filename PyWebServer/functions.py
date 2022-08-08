6db9f8610e8b52205d02dd13d8011523b401cdef
欣普 <15877161799@163.com>
2022-08-09 00:26:19 +0800

    更新对于h2支持的函数

--- PyWebServer/functions.py
+++ PyWebServer/functions.py
@@ -2,8 +2,60 @@ import inspect, ctypes
 from io import *
 import gzip
 import hashlib
+import time, os
 
-HashCacheSize = 4096
+HashCacheSize = 4096*1024*2
+LOG = False
+NOT_RECORDED_FUNCTIONS = ['parsingHeaderLine']
+PluginManager = None
+
+def Functions_SetPluginManager(PluginManager):
+    globals()["PluginManager"] = PluginManager
+
+def toObject(dic):
+    class Module:
+        def __str__(self):
+            return '<Module Object>'
+        pass
+    a = Module()
+    for k in dic:
+        v = dic[k]
+        a.__setattr__(k, v)
+    return a
+#日志函数
+def profile(func):
+    def x(*arg, **args):
+        PluginManager.FindWay(func.__name__, arg, args)
+
+        if not func.__name__ in NOT_RECORDED_FUNCTIONS and LOG:
+            l = time.time()
+            o = func(*arg, **args)
+            p = time.time()
+            with open("logs/log.txt", 'a') as f:
+                f.write(time.ctime()+"-->:函数"+func.__name__+" 耗时:"+str(p-l)+";返回值:"+str(o)+"\n")
+        else:
+            o = func(*arg, **args)
+        return o
+    return x
+def setLog(content, file='./logs/view.log', showTime=True):
+    with open(file, 'a', encoding='utf-8') as f:
+        x = "["+time.ctime()+"] "
+        if not showTime:
+            x = ''
+        f.write(x+content+"\n")
+#报错查询具体位置
+def tryGetErrorDetail(s, path):
+    with open(path, 'r', encoding='UTF-8') as f:
+        try:
+            x=s.split("line")[-1].split(",")[0]
+            x = int(x.strip())
+        except:
+            return s
+        for i in range(x):
+            ctx = f.readline()
+            if i+1 == x:
+                return s+"<br><br>FileName:%s<br><br>%s: %s"%(path, x, ctx)
+        return s
 
 #Gzip 压缩函数
 def gzip_encode(data):
@@ -120,9 +172,17 @@ def iniToJson(inipath):
     return result
 
 #洁净输出json
-def prettyPrint(j):
-    import json
-    print(json.dumps(j, sort_keys=True, indent=4, separators=(',', ':')))
+def prettyPrint(j, ret=False):
+    if isinstance(j, dict):
+        import json
+        res = (json.dumps(j, sort_keys=True, indent=4, separators=(',', ':')))
+        if ret:
+            return res
+        else:
+            print(res)
+    elif isinstance(j, list):
+        #print(j)
+        print("   \n".join([(prettyPrint(i, 1) if isinstance(i, dict) else str(i)) for i in j]))
 
 #是否是一个ipv4地址
 def isIPv4(domain):
@@ -155,3 +215,34 @@ def isNum(st):
         return False
     else:
         return int(st)
+
+#将[(key, val), ...]类型的list转为dict
+def kvListToDict(v):
+    d = {}
+    for i in v:
+        d[i[0]] = i[1]
+    return d
+
+#获取一个字节中某一位比特
+def getBit(byteNum, i, byteorder):
+    return (byteNum >> (i if byteorder == 'little' else 7-i)) & 1
+def getRangeBit(b, from_, to, byteorder):
+    x = ''
+    for i in b:
+        for n in (list(range(8)) if byteorder == 'little' else list(range(8))[::-1]):
+            x += str(getBit(i, n, 'little'))
+    return x[from_: to]
+
+def toH1Format(x):
+    p = {}
+    for i in x:
+        if i == ':authority':
+            p['host'] = x[i]
+        elif i == ':path':
+            p['path'] = x[i]
+        elif i == ':method':
+            p['method'] = x[i]
+        else:
+            p[i] = x[i]
+
+    return p
