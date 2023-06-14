0846f1f25ec316f69ad922db1d66ca68b2bd8749
欣普 <15877161799@163.com>
2023-06-15 00:26:50 +0800

    线程锁的更新

--- PyWebServer/DispatchThread.py
+++ PyWebServer/DispatchThread.py
@@ -9,27 +9,31 @@ STOP = False
 ThreadLock = threading.Lock()
 
 class Task(object):
-    def __init__(self, priority, obj):
+    def __init__(self, priority, obj, useLock):
         self.priority = priority
         self.obj      = obj
+        self.useLock  = useLock
     def __str__(self):
-        return f"Task(priority={self.priority}, object={self.obj})"
+        return f"Task(object={self.obj} priority={self.priority})"
     def __lt__(self, other):
         return self.priority < (other.priority if 'priority' in other.__dir__() else 3)
 
-    
+def lock():
+    ThreadLock.acquire()
+def release():
+    ThreadLock.release()
 
-def addThread(obj, priority=1):
-    ThreadPool.put(Task(priority, obj))
+def addThread(obj, priority=1, useLock=True):
+    ThreadPool.put(Task(priority, obj, useLock))
 
 def work():
     global ThreadPool, STOP
     while not STOP:
         thread = ThreadPool.get()
-        
-        ThreadLock.acquire()
+        print(thread, thread.useLock)
+        lock() if thread.useLock else 0
         thread.obj.start()
-        ThreadLock.release()
+        release() if thread.useLock else 0
 
 worker = threading.Thread(target=work)
 worker.start()
