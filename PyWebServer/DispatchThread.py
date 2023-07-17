24f28a2ab55b98b0e165ed4f46cf1ae1cec35746
欣普 <15877161799@163.com>
2023-07-18 00:30:33 +0800

    Update DispatchThread.py

--- PyWebServer/DispatchThread.py
+++ PyWebServer/DispatchThread.py
@@ -1,11 +1,10 @@
+
 import queue
 import threading
+from ServerConfig import config
 
-
-from server_config import config
-"""调度模块"""
 ThreadPool = queue.PriorityQueue(config['threadpool-maxsize'])
-STOP = False
+STOP       = False
 ThreadLock = threading.Lock()
 
 class Task(object):
@@ -14,26 +13,43 @@ class Task(object):
         self.obj      = obj
         self.useLock  = useLock
     def __str__(self):
-        return f"Task(object={self.obj} priority={self.priority})"
+        return f"{{self.__class__.__name__}}(object={self.obj} priority={self.priority})"
+    def __repr__(self):
+        return self.__str__()
     def __lt__(self, other):
-        return self.priority < (other.priority if 'priority' in other.__dir__() else 3)
+        return self.priority > (other.priority if 'priority' in other.__dir__() else 3)
+
+locks = {}
+
+def newLock(id) -> threading.Lock:
+    if not id in locks.keys():
+        locks[id] = threading.Lock()
+    return locks[id]
 
 def lock():
     ThreadLock.acquire()
 def release():
     ThreadLock.release()
 
-def addThread(obj, priority=1, useLock=True):
+def addThread(obj, priority=1, useLock=False):
     ThreadPool.put(Task(priority, obj, useLock))
+
+def useThread(priority=1, useLock=False):
+    def useThread_(func):
+        def run(*arg, **args):
+            t = threading.Thread(target=func, args=arg, kwargs=args)
+            addThread(t, priority, useLock)
+        return run
+    return useThread_
 
 def work():
     global ThreadPool, STOP
     while not STOP:
         thread = ThreadPool.get()
-        print(thread, thread.useLock)
         lock() if thread.useLock else 0
         thread.obj.start()
         release() if thread.useLock else 0
 
-worker = threading.Thread(target=work)
-worker.start()
+def init():
+    worker = threading.Thread(target=work)
+    worker.start()
