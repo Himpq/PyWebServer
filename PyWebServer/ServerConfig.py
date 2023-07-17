e607887f995bd67673ebff90b52abed3bb487599
欣普 <15877161799@163.com>
2023-07-18 00:30:06 +0800

    Update and rename server_config.py to ServerConfig.py

--- PyWebServer/ServerConfig.py
+++ PyWebServer/ServerConfig.py
@@ -1,5 +1,6 @@
+
 import os
-from functions import iniToJson, prettyPrint
+from Functions import iniToJson, prettyPrint
 
 if os.path.isdir(",/temp"):
     os.mkdir("./temp")
@@ -17,7 +18,6 @@ ssl=false
 ssljump-domain=localhost
 
 [config]
-language-wrong-error=false
 ranges-download=true
 default-page=('index.html','index.htm','index.py')
 python=true
@@ -25,28 +25,23 @@ raise-error=true
 timeout=5
 keep-alive-max=20
 cachesize=409600
-sameip-request-count=5
 server-status=NORMAL
 errorpage-path=./ErrorPages/error.html
 ssl-path=('cert.crt','key.key','ca.crt')
 ssl-password=
-ssl-doshakehand=true
-maxsize-for-etag = (1024*1024*100)
-enable-plugin=true
+ssl-dohandshake=true
+maxsize-for-etag=(1024*1024*100)
 support-protocols=(["spdy/3.1", "http/1.1"])
-encoding=(['gzip', 'identity'])
 threadpool-maxsize=100
+collection-expire-time=300
+session-expire-time=600
 
 [black_list]
 blacklist=
 
-[bind_domains]
-domains=
-
 [http_errorcodes]
 503=('Server Unavailable','Server overload or maintenance in progress. Please wait for administrator to maintain or visit later.')
 404=('Not Found','Page is not found.')
-406=('Language Error','The language is not supported by the server.')
 safeguard=('Server Safeuard','The server is being maintained.')
 
 [headers]
@@ -59,7 +54,14 @@ SETTINGS_INITAL_WINDOW_SIZE=65535
 SETTINGS_MAX_FRAME_SIZE=16384
 SETTINGS_MAX_HEADER_LIST_SIZE=16384
 
+[logger]
+# types: complete, info, warning, error
+ignore_list=([])
+
 """.format(os.path.abspath("./Website"), os.path.abspath("./")))
+
+ServerPath = None
+InstallPath = None
 
 opts = iniToJson("./pws_config.ini")
 
@@ -89,18 +91,20 @@ for i in opts['http_errorcodes']:
 for i in opts['HTTP2']:
     http2settings[i] = int(opts['HTTP2'][i])
 
+logger = opts['logger']
+
 
 black_list = [] #IP黑名单
 bind_domains = [] #绑定的域名
 
 
-ERRPagePath = "./ErrorPages/error.html"
+ERRPagePath = config['errorpage-path']
 ERRPageStr = open(ERRPagePath,'r').read()
 ERRPage = lambda:ERRPageStr
 
 def __printContent():
     import json
     print(json.dumps(opts, sort_keys=True, indent=4, separators=(',', ':')))
-    print(config)
 
-#__printContent()
+if __name__ == "__main__":
+    __printContent()
